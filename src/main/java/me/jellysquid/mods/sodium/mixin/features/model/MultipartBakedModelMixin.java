0d8b9893fca0c546db002f03f735116caf653271
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-24 10:38:41 -0300

    Update MultipartBakedModelMixin.java

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/model/MultipartBakedModelMixin.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/model/MultipartBakedModelMixin.java
@@ -8,16 +8,17 @@ import net.minecraft.client.render.model.MultipartBakedModel;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import org.apache.commons.lang3.tuple.Pair;
-import org.spongepowered.asm.mixin.*;
-
-import java.util.*;
-import java.util.concurrent.locks.StampedLock;
-import java.util.function.Predicate;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(MultipartBakedModel.class)
 public class MultipartBakedModelMixin {
+
     @Unique
-    private final Map<BlockState, BakedModel[]> stateCacheFast = new Reference2ReferenceOpenHashMap<>();
+    private static final int MAX_CACHE_SIZE = 1024;
+
+    @Unique
+    private final Map<Pair<BlockState, Direction>, BakedModel[]> stateCacheFast = new Reference2ReferenceOpenHashMap<>(MAX_CACHE_SIZE);
     @Unique
     private final StampedLock lock = new StampedLock();
 
@@ -31,15 +32,17 @@ public class MultipartBakedModelMixin {
      */
     @Overwrite
     public List<BakedQuad> getQuads(BlockState state, Direction face, Random random) {
+
         if (state == null) {
             return Collections.emptyList();
         }
 
+        Pair<BlockState, Direction> key = Pair.of(state, face);
         BakedModel[] models;
 
         long readStamp = this.lock.readLock();
         try {
-            models = this.stateCacheFast.get(state);
+            models = this.stateCacheFast.get(key);
         } finally {
             this.lock.unlockRead(readStamp);
         }
@@ -51,25 +54,25 @@ public class MultipartBakedModelMixin {
 
                 for (Pair<Predicate<BlockState>, BakedModel> pair : this.components) {
                     if (pair.getLeft().test(state)) {
-                        modelList.add(pair.getRight());
+                        if (pair.getRight().isFaceVisible(face)) {
+                            modelList.add(pair.getRight());
+                        }
                     }
                 }
 
                 models = modelList.toArray(BakedModel[]::new);
-                this.stateCacheFast.put(state, models);
+                this.stateCacheFast.put(key, models);
             } finally {
                 this.lock.unlockWrite(writeStamp);
             }
         }
 
         List<BakedQuad> quads = new ArrayList<>();
-        long seed = random.nextLong();
-
         for (BakedModel model : models) {
-            random.setSeed(seed);
             quads.addAll(model.getQuads(state, face, random));
         }
 
         return quads;
     }
+
 }
